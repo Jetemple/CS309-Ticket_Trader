6f600b47d54b59e79397ecc4d8a87f8eebf6fbe8
Justin Worley <jmworley@iastate.edu>
2019-01-27 21:30:20 -0600

    added business class

---
+++ Experiments/justin/Experimenting/src/main/java/com/example/demo/PeopleService.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PeopleService {
+	List<People> people = Arrays.asList(
+			new People("1234","JOE","06272000"),
+			new People("4321","BOB","0000"),
+			new People("9999","Hell","666666666")
+			);
+	
+	
+	public List<People> getAllPeople(){
+		return people;
+	}
+	}
