88223c2d378263effe9deb8a62d87104e154e223
Justin Worley <jmworley@iastate.edu>
2019-01-27 21:49:12 -0600

    played with first get

--- Experiments/justin/Experimenting/src/main/java/com/example/demo/PeopleController.java
+++ Experiments/justin/Experimenting/src/main/java/com/example/demo/PeopleController.java
@@ -1,22 +1,27 @@
 package com.example.demo;
 
-
-import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class peopleData {
+public class PeopleController {
+
 	@Autowired
 	private PeopleService peopleService;
-	
+
 	@RequestMapping("/People")
 	public List<People> getAllPeople() {
 		return peopleService.getAllPeople();
-
 	}
-
+	@RequestMapping("/People/{SSN}")
+	public People getPeople(@PathVariable String SSN) {
+		return peopleService.getPeople(SSN);
+		}
+	
+	
+	
 }
--- Experiments/justin/Experimenting/src/main/java/com/example/demo/PeopleService.java
+++ Experiments/justin/Experimenting/src/main/java/com/example/demo/PeopleService.java
@@ -8,13 +8,16 @@ import org.springframework.stereotype.Service;
 @Service
 public class PeopleService {
 	List<People> people = Arrays.asList(
-			new People("1234","JOE","06272000"),
-			new People("4321","BOB","0000"),
-			new People("9999","Hell","666666666")
+			new People("1234", "JOE", "06272000"), 
+			new People("4321", "BOB", "0000"),
+			new People("9999", "Hell", "666666666")
 			);
-	
-	
-	public List<People> getAllPeople(){
+
+	public List<People> getAllPeople() {
 		return people;
 	}
+	
+	public People getPeople(String SSN) {
+		return people.stream().filter(t -> t.getSSN().equals(SSN)).findFirst().get();
 	}
+}
