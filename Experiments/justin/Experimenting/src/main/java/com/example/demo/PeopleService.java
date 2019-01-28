06732fa08993a494f0b322e2ed2038c9be19b03a
Justin Worley <jmworley@iastate.edu>
2019-01-27 23:01:13 -0600

    playing with updating locally

--- Experiments/justin/Experimenting/src/main/java/com/example/demo/PeopleService.java
+++ Experiments/justin/Experimenting/src/main/java/com/example/demo/PeopleService.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -7,11 +8,12 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class PeopleService {
-	List<People> people = Arrays.asList(
+	
+	private static List<People> people = new ArrayList<>(Arrays.asList(
 			new People("1234", "JOE", "06272000"), 
 			new People("4321", "BOB", "0000"),
 			new People("9999", "Hell", "666666666")
-			);
+			));
 
 	public List<People> getAllPeople() {
 		return people;
@@ -19,5 +21,21 @@ public class PeopleService {
 	
 	public People getPeople(String SSN) {
 		return people.stream().filter(t -> t.getSSN().equals(SSN)).findFirst().get();
+	}
+
+	public static void addPeople(People people2) {
+		people.add(people2);
+		
+	}
+	//Updates the persons name. Used a for loop to find the correct array to update.
+	public static void updatePeople(String name, People people2) {
+		for(int i=0; i<people.size(); i++) {
+			People t= people.get(i);
+			if(t.getName().equals(name)) {
+				people.set(i, people2);
+				return;
+			}
+		}
+		
 	}
 }
