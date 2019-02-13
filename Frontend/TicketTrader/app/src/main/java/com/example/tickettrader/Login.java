e8a4e83a20678a901d2c0ebb6a32babe012ca8e2
Ali <almadani@iastate.edu>
2019-02-12 19:56:13 -0600

    Hi

--- Frontend/TicketTrader/app/src/main/java/com/example/tickettrader/Login.java
+++ Frontend/TicketTrader/app/src/main/java/com/example/tickettrader/Login.java
@@ -8,23 +8,45 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.android.volley.Cache;
+import com.android.volley.Network;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.BasicNetwork;
+import com.android.volley.toolbox.DiskBasedCache;
+import com.android.volley.toolbox.HurlStack;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.JsonRequest;
+import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONObject;
+
 public class Login extends AppCompatActivity {
 
+    final private String server_URL = "https://cs309-pp-1.misc.iastate.edu:8080";
     private EditText Name;
     private EditText Password;
     private TextView Info;
     private Button Login;
-    private int Counter = 3;
+    private int Counter;
+    RequestQueue requestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
 
+        Counter = 3;
         Name = (EditText) findViewById(R.id.etName);
         Password = (EditText) findViewById(R.id.etPassword);
         Info = (TextView) findViewById(R.id.tvInfo);
         Login = (Button) findViewById(R.id.btnLogin);
+        Cache cache = new DiskBasedCache(getCacheDir(), 1024*1024);
+        Network network = new BasicNetwork(new HurlStack());
+        requestQueue = new RequestQueue(cache, network);
+        requestQueue.start();
 
         Info.setText("# of attempts remaining: 3");
 
@@ -40,6 +62,26 @@ public class Login extends AppCompatActivity {
     }
 
     private void validate(String userName, String userPassword){
+
+        JsonObjectRequest stringRequest = new JsonObjectRequest(Request.Method.POST, server_URL, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Info.setText(response.toString());
+                        requestQueue.stop();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Info.setText("Something went wrong........");
+                        error.printStackTrace();
+                    }
+                }
+        );
+
+        requestQueue.add(stringRequest);
+
         if(userName.equals("Admin") && (userPassword.equals("password")))
         {
             Intent intent = new Intent(com.example.tickettrader.Login.this, SecondActivity.class);
