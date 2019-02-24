48bc13e24a1c0ef66726be722e901c595a8906f1
Justin <jmworley@iastate.edu>
2019-02-24 00:22:28 -0600

    Added get by location

--- Backend/src/main/java/io/pp1/tickets/TicketController.java
+++ Backend/src/main/java/io/pp1/tickets/TicketController.java
@@ -4,13 +4,10 @@ package io.pp1.tickets;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,37 +16,45 @@ public class TicketController {
 	
 	@Autowired
 	private TicketRepository ticketRepository;
-	//private TicketService ticketService;
 	
 	@RequestMapping(method = RequestMethod.GET, path = "/tickets")
 	public List<Ticket> getAll(){
-		return ticketRepository.findAll();
-		
+		return ticketRepository.findAll();	
 	}
-	
-	@RequestMapping(method = RequestMethod.POST, path = "/tickets") //@PostMapping(value = "/tickets")
-	public void persist(@RequestBody final Ticket ticket){
-		ticketRepository.save(ticket);
-	}
-	
 	
     @RequestMapping(method = RequestMethod.GET, path = "/tickets/{ticket_id}")
     public Ticket getTicket(@PathVariable Integer ticket_id) {
     	return (Ticket) ticketRepository.getTicketByID(ticket_id);
     }
     
-    @RequestMapping(method = RequestMethod.GET, path = "/tickets/getbydate/{game_date}")
+    @RequestMapping(method = RequestMethod.GET, path = "/tickets/sport/{sport}")
+    public List<Ticket> getBySport(@PathVariable String sport) {
+    	return ticketRepository.getTicketBySport(sport);
+    }
+    
+    @RequestMapping(method = RequestMethod.GET, path = "/tickets/date/{game_date}")
     public List<Ticket> getByDate(@PathVariable String game_date) {
     	return  ticketRepository.getTicketByDate(game_date);
     }
     
+    @RequestMapping(method = RequestMethod.GET, path = "/tickets/location/{game_location}")
+    public List<Ticket> getByLocation(@PathVariable String game_location) {
+    	return ticketRepository.getTicketByLocation(game_location);
+    }
+    
+    @RequestMapping(method = RequestMethod.GET, path = "/tickets/opponent/{opponent}")
+    public List<Ticket> getByOpponent(@PathVariable String opponent) {
+    	return ticketRepository.getTicketByOpponent(opponent);
+    }
+    
+	@RequestMapping(method = RequestMethod.POST, path = "/tickets") //@PostMapping(value = "/tickets")
+	public void persist(@RequestBody final Ticket ticket){
+		ticketRepository.save(ticket);
+	}
+	
     @RequestMapping(method = RequestMethod.DELETE, path = "/tickets/{ticket_id}")
     public void delete(@PathVariable Integer ticket_id) {
     	ticketRepository.deleteById(ticket_id);
     }
     
-    @RequestMapping(method = RequestMethod.GET, path = "/tickets/sport/{sport}")
-    public List<Ticket> getBySport(@PathVariable String sport) {
-    	return ticketRepository.getTicketBySport(sport);
-    }
 }
--- Backend/src/main/java/io/pp1/tickets/TicketRepository.java
+++ Backend/src/main/java/io/pp1/tickets/TicketRepository.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -21,6 +20,12 @@ public interface TicketRepository extends JpaRepository<Ticket, Integer> {
 
 	@Query(value = "SELECT * FROM ticket u WHERE u.sport = ?1", nativeQuery=true)
 	List<Ticket> getTicketBySport(String sport);
+
+	@Query(value = "SELECT * FROM ticket u WHERE u.opponent = ?1", nativeQuery=true)
+	List<Ticket> getTicketByOpponent(String opponent);
+
+	@Query(value = "SELECT * FROM ticket u WHERE u.game_location = ?1", nativeQuery=true)
+	List<Ticket> getTicketByLocation(String game_location);
 
 //	@Query(value = "DELETE FROM ticket u WHERE  u.ticket_id = ?1", nativeQuery=true)
 //	List<Ticket> removeByID(Integer ticket_id);
