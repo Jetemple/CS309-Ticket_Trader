c51704263e7b604ae930dc798a265ef31f3e9346
Justin <jmworley@iastate.edu>
2019-02-26 10:06:47 -0600

    Added Omar's User Version

--- Backend/src/main/java/io/pp1/users/UserController.java
+++ Backend/src/main/java/io/pp1/users/UserController.java
@@ -1,28 +1,37 @@
 package io.pp1.users;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+
+
+import java.util.List;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
 	
-	
 	@Autowired
-	private UserRepository userRepository;
+	private UserService userService;
 	
 	@GetMapping(value = "/users")
-	public UserService getAll(){
-		return new UserService(userRepository.findAll());
+	public List<User> getAll(){
 		
+		return userService.getUsers();
+	}
+	
+	@GetMapping("/users/{id}")
+	public boolean userExist(@PathVariable Integer id) {
+		
+		
+		return userService.userExist(id);
 	}
 	
 	@PostMapping(value = "/users")
-	public void persist(@RequestBody final User users){
-		userRepository.save(users);
-
+	public void post(@RequestBody final User users){
+		userService.post(users);
 	}
 
 }
--- Backend/src/main/java/io/pp1/users/UserRepository.java
+++ Backend/src/main/java/io/pp1/users/UserRepository.java
@@ -1,12 +1,13 @@
 package io.pp1.users;
 
-import java.util.List;
+
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Integer> {
+public interface UserRepository extends CrudRepository<User, Integer> {
 
-	List<User> findAll();
+	
 }
