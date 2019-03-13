28fc2e94870b10bda676c16c5cba79e41fb4687a
Justin <jmworley@iastate.edu>
2019-03-12 22:28:49 -0500

    Added a filter option by sport, date, and opponent. Also make the ticket go from highest  to lowest ticket_id

--- Backend/src/main/java/io/pp1/logos/LogoRepository.java
+++ Backend/src/main/java/io/pp1/logos/LogoRepository.java
@@ -3,6 +3,7 @@ package io.pp1.logos;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 
